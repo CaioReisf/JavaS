e0140bdbec842778c3d9b0338b241e16ae178eba
caio reis <caioreis00p@gmail.com>
2025-05-12 10:03:50 -0300

    up

--- modolo C/ex004.js
+++ modolo C/ex004.js
@@ -1,4 +1,8 @@
 /*<!--{} t `` []-->*/
 
-var vel= 60.5
-console.log
+var vel= 120
+console.log(`a velocidade do eu carro eh de ${vel} km/h`)
+if (vel > 60){ //condiçao simples
+    console.log(`passou o limite de velocidade. MULTADO!!`)
+}
+console.log (`dirija usando cinto de segurança`)
---
+++ modolo C/ex005.js
@@ -0,0 +1,9 @@
+/*<!--{} t `` []-->*/
+
+var pais = 'EUA'
+console.log(`morando em ${pais}`)
+if (pais =='Brasil'){
+    console.log('tu é braileira')
+}else{
+    console.log('tu é estrageira')
+}
