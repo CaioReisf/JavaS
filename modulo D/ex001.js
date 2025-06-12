24b05a583ab0702e05121c4989208b80517d04e1
caio reis <caioreis00p@gmail.com>
2025-06-12 17:08:00 -0300

    up

---
+++ modulo D/ex001.js
@@ -0,0 +1,6 @@
+var vel = 100
+console.log(`a velocidade do seu carro eh de ${vel}km/h`)
+if(vel >=60){ //Condicoao simples !
+    console.log(`voce ultrapassou a velocidade permitida, MULTADO!`)
+}
+console.log(`dirrija usando cinto de seguranca!`)
---
+++ modulo D/ex003.js
@@ -0,0 +1,11 @@
+var idade = 77
+if (idade <16){
+    console.log(`nao vota`)
+}else{
+   if (idade >=16 && idade <18 || idade >65){
+    console.log(`voto opcional`)
+   } else{
+    console.log(`voto obrigatorio`)
+   }
+
+}
