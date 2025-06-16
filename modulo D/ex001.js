00798efd9f72110c0a473b4d2d1495036e6b3d6e
caio reis <caioreis00p@gmail.com>
2025-06-16 15:20:41 -0300

    up

--- modulo D/ex001.js
+++ modulo D/ex001.js
@@ -3,4 +3,5 @@ console.log(`a velocidade do seu carro eh de ${vel}km/h`)
 if(vel >=60){ //Condicoao simples !
     console.log(`voce ultrapassou a velocidade permitida, MULTADO!`)
 }
-console.log(`dirrija usando cinto de seguranca!`)
+console.log(`dirrija usando cinto de seguranca!`     )
+
