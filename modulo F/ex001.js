b4cfcb80df1ce8e3db9c382220b1415534971e46
caio reis <caioreis00p@gmail.com>
2025-07-01 22:59:36 -0300

    up

---
+++ modulo F/ex001.js
@@ -0,0 +1,7 @@
+let num = [5,8,9,3]
+num.push(7)
+
+console.log(`nosso vetor e o ${num}`)
+
+//num.length para ver o comprimento do veitor
+//num.sort() ele coloca os elementos em ordem crescente
---
+++ modulo F/ex002.js
@@ -0,0 +1,18 @@
+let valores = [8,1,7,4,2,9]
+
+for(let pos=0; pos<valores.length; pos++){
+    console.log (`a posicao ${pos} tem o valor ${valores[pos]}`)
+
+}
+
+
+//forma moderna:
+
+
+//let valores = [8,1,7,4,2,9]
+
+//for(let pos in valores){
+//    console.log (`a posicao ${pos} tem o valor ${valores[pos]}`)
+
+//}
+
